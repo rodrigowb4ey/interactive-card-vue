c69cdc03b57928469e52e01f607bb2cf352fdc9b
Rodrigo Saraiva <rodrigobezerrasaraiva@gmail.com>
2023-01-06 12:29:07 -0300

    Positioning the Card faces in the container

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,19 @@
 <template>
-  <main>
-    <section id="card-faces" class="flex">
-      <img src="/assets/images/bg-card-back.png" alt="Card Back" />
-      <img src="/assets/images/bg-card-front.png" alt="Card Front" />
+  <main class="flex border-4 border-yellow-500">
+    <section
+      id="card-faces"
+      class="flex flex-col justify-around place-items-center border-2 border-blue-700 h-full w-1/2"
+    >
+      <img
+        class="flex h-fit w-fit pr-16"
+        src="/assets/images/bg-card-front.png"
+        alt="Card Front"
+      />
+      <img
+        class="flex h-fit w-fit pl-16"
+        src="/assets/images/bg-card-back.png"
+        alt="Card Back"
+      />
     </section>
     <form action=""></form>
   </main>
